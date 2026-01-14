1b5910cb73d2d825d3bdfb30bd9d70e570556e10
maniram <maniramyadav786@gmail.com>
2026-01-14 16:17:19 +0000

    image generation added

--- AgenticChatbot/src/langgraphagenticai/main.py
+++ AgenticChatbot/src/langgraphagenticai/main.py
@@ -3,10 +3,11 @@ from src.langgraphagenticai.ui.streamlitui.loadui import LoadStreamlitUI
 
 def load_langgraph_agenticai_app():
     """
-        Load and runs Loangraph AgenticAI
-        ----
+        Loads and runs the LangGraph AgenticAI application with Streamlit UI.
+        This function initializes the UI, handles user input, configures the LLM model,
+        sets up the graph based on the selected use case, and displays the output while 
+        implementing exception handling for robustness.
     """
-
     #Load UI
     ui = LoadStreamlitUI()
     user_input = ui.load_streamlit_ui()
@@ -16,4 +17,3 @@ def load_langgraph_agenticai_app():
         return
     
     user_message = st.chat_input("Enter your message : ")
-    
