9b2a4c54fd5c5b242b913aa9dcfb8b45a2ae358d
maniram <maniramyadav786@gmail.com>
2026-01-16 12:08:36 +0000

    ai news ui implemntation

--- AgenticChatbot/src/langgraphagenticai/ui/streamlitui/loadui.py
+++ AgenticChatbot/src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -23,7 +23,7 @@ class LoadStreamlitUI:
             usecase_options = self.config.get_usecase_options()
 
             # LLM selections
-            self.user_controls["selected_llm"] = st.selectbox("Selcct LLM",llm_options)
+            self.user_controls["selected_llm"] = st.selectbox("Select LLM",llm_options)
 
             if self.user_controls["selected_llm"] == 'Groq':
                 #Model selection
@@ -38,13 +38,30 @@ class LoadStreamlitUI:
             # Usecase selection
             self.user_controls["selected_usecase"]=st.selectbox("Select Usecases",usecase_options)
 
-            if self.user_controls["selected_usecase"]=="Chatbot with Tool":
+            if self.user_controls["selected_usecase"]=="Chatbot with Tool" or self.user_controls["selected_usecase"] =="AI News" :
+
                 os.environ["TAVILY_API_KEY"] = self.user_controls["TAVILY_API_KEY"] = st.session_state["TAVILY_API_KEY"] = st.text_input("TAVILY API KEY",
                                                                                         type="password")
-                            # Validate API key
+                # Validate API key
                 if not self.user_controls["TAVILY_API_KEY"]:
                     st.warning("⚠️ Please enter your TAVILY_API_KEY key to proceed. Don't have? refer : https://app.tavily.com/home")
+                
+                elif self.user_controls['selected_usecase']=="AI News":
+                    st.subheader("AI News Explorer")
 
+                    with st.sidebar:
+                        time_frame = st.selectbox("📅 Select Time Frame",
+                            ["Daily", "Weekly", "Monthly"],
+                            index=0)
+                    
+                    if st.button("🔍 Fetch Latest AI News", use_container_width=True):
+                        st.session_state.IsFetchButtonClicked = True
+                        st.session_state.timeframe = time_frame
+                    else :
+                        st.session_state.IsFetchButtonClicked = False
+                # if "state" not in st.session_state:
+                #     st.session_state.state = self.initialize_session()
+            
         return self.user_controls
 
 
