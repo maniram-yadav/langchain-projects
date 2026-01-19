c6bf72e6305f6a1ba9f977098ece02a76601557a
maniram <maniramyadav786@gmail.com>
2026-01-19 16:20:33 +0000

    facebook vidoe uplaod from url added

--- blogagentic/app.py
+++ blogagentic/app.py
@@ -9,6 +9,9 @@ load_dotenv()
 
 app = FastAPI()
 os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ['LANGCHAIN_TRACING_V2']='true'
+os.environ['LANGCHAIN_PROJECT']="Blog App"
 
 ## APIs
 
